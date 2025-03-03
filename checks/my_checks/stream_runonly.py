d8b49be5538ef9ac63284f426531045e13fb1cb6
Philip P. Müller, LUMI <philip.mueller@cscs.ch>
2025-03-03 09:48:25 +0200

    Added my dummy test.
    It can be run through:
    ```
    	reframe -C config/lumi.py  -c checks/my_checks/stream_runonly.py --keep -o TEST_RUN -r
    ```
    However for this the venv must be loaded, if pyenv is used then it will
    simply work.

---
+++ checks/my_checks/stream_runonly.py
@@ -0,0 +1,27 @@
+# Copyright 2016-2024 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class stream_test(rfm.RunOnlyRegressionTest):
+    valid_systems = ['*:small']
+    valid_prog_environs = ['PrgEnv-gnu']
+    executable = './stream.x'
+
+    #extra_resources = { 'memory': {'mem_per_node': 1000}}
+
+    @sanity_function
+    def validate(self):
+        return sn.assert_found(r'Solution Validates', self.stdout)
+
+    @performance_function('MB/s')
+    def copy_bw(self):
+        return sn.extractsingle(r'Copy:\s+(\S+)', self.stdout, 1, float)
+
+    @performance_function('MB/s')
+    def triad_bw(self):
+        return sn.extractsingle(r'Triad:\s+(\S+)', self.stdout, 1, float)
