8b234d36f454c53683f440455a3ce9a339c46ce7
Philip Mueller <philip.mueller@cscs.ch>
2025-03-03 10:05:17 +0100

    Added a script that is able to post a message on Rocket-Chat.

---
+++ scripts/run_reporter.py
@@ -0,0 +1,104 @@
+#!/bin/env python
+# GT4Py - GridTools Framework
+#
+# Copyright (c) 2014-2024, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""This script reports the result of the ReFrame test to the Rocket-Chat."""
+
+from typing import Any, Optional
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+from rocketchat_API.rocketchat import RocketChat
+
+
+USER_ID: str = ""
+"""ID of the user that should make the post."""
+
+ROOM_ID: str = ""
+"""The ID of the room where the massage should be posted."""
+
+SERVER_URL: str = "https://chat.csc.fi"
+"""The URL where the Rocket-Chat server is running on."""
+
+
+def main(
+    API_token: Optional[str],
+    message_file: Path,
+) -> int:
+    """Post the content of `message_file` on the Rocket-Chat.
+
+    Args:
+        API_token: The token that should be used to authenticate the posting.
+            If not given it will be read from the variable `ROCKET_CHAT_TOKEN`.
+        message_file: The file containing the body of the message.
+    """
+    assert USER_ID, "The `USER_ID` has not been configured."
+    assert ROOM_ID, "The `ROOM_ID` has not been configured."
+
+    if not message_file.exists():
+        raise FileNotFoundError(
+            f"The file containing the message's body, '{message_file}', could not be located."
+        )
+
+    if not API_token:
+        try:
+            API_token = os.environ.get("ROCKET_CHAT_TOKEN")
+        except KeyError:
+            raise RuntimeError(
+                "The API token was not given and `ROCKET_CHAT_TOKEN` was not set."
+            ) from None
+
+    with open(message_file, "rt") as F:
+        message_body = "".join(F.readlines())
+
+    rocket = RocketChat(
+        user_id=USER_ID,
+        auth_token=API_token,
+        server_url=SERVER_URL,
+    )
+
+    result: dict[str, Any] = rocket.chat_post_message(
+        message_body,
+        room_id=ROOM_ID,
+    ).json()
+
+    if result.get("success", False):
+        return 0
+    else:
+        raise RuntimeError(
+            "Failed to successfully submit the post to {SERVER_URL} with error: {result.get('error', 'Unknown error')}"
+        )
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(prog="run_reporter.py")
+
+    args.add_argument(
+        "--api-token",
+        dest="API_token",
+        help="The API token that should be used for authentification, if not given read from `ROCKET_CHAT_TOKEN`.",
+        type=str,
+    )
+    args.add_argument(
+        "--message-file",
+        dest="message_file",
+        help="File containing the body of the post that should be made.",
+        type=Path,
+    )
+
+    parsed_args = args.parse_args()
+
+    sys.exit(
+        main(
+            API_token=parsed_args.API_token,
+            message_file=parsed_args.message_file,
+        )
+    )
